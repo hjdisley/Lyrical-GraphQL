9a61827ffe80dc794bfd6e5faed6936ac41f9ffb
Hayden Disley <hayden@pc-6-209.customer.ask4.lan>
2021-12-13 20:55:35 +0000

    set up react router

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,31 @@
 import React from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = ({ data: { error, loading, songs } }) => {
+  if (loading) {
+    return <div>loading</div>;
+  }
+  return (
+    <div>
+      <ul className="collection">
+        {songs.map((song) => (
+          <li className="collection-item" key={song.id}>
+            {song.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
